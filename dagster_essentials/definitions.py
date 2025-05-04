fc4c978b08be67235bdb34bf1cca026fd912a3b9
shubham23471 <shubhamshakya61@gmail.com>
2025-05-05 00:06:53 +0530

    created jobs and added schedules

--- dagster_essentials/definitions.py
+++ dagster_essentials/definitions.py
@@ -1,10 +1,18 @@
 import dagster as dg
-
 from dagster_essentials.assets import metrics, trips
 from dagster_essentials.resources import database_resource
+from dagster_essentials.jobs import trip_update_job, weekly_update_job
+from dagster_essentials.schedules import trip_update_schedule, weekly_update_schedule
 
+# loading assets
 trip_assets = dg.load_assets_from_modules([trips])
 metric_assets = dg.load_assets_from_modules([metrics])
+
+# loading jobs
+all_jobs = [trip_update_job, weekly_update_job]
+
+# loading schedules
+all_schedule = [trip_update_schedule, weekly_update_schedule]
 
 # When loading a code location, Dagster looks for a variable that contains a Definitions object. 
 # i.e: defs variable
@@ -13,4 +21,6 @@ defs = dg.Definitions(
     resources={
         "database": database_resource,
     },
+    jobs=all_jobs,
+    schedules=all_schedule
 )
--- steps.md
+++ steps.md
@@ -18,20 +18,3 @@ error: Project is already initialized in `/Users/shubham/projects/learning-dagst
 
 
 export DAGSTER_HOME=/Users/shubham/projects/learning-dagster/project-dagster-university/dagster_university/dagster_essentials/dagster_home
-
-
-
-create or replace table trips as (
-    select
-        VendorID as vendor_id,
-        PULocationID as pickup_zone_id,
-        DOLocationID as dropoff_zone_id,
-        RatecodeID as rate_code_id,
-        payment_type as payment_type,
-        tpep_dropoff_datetime as dropoff_datetime,
-        tpep_pickup_datetime as pickup_datetime,
-        trip_distance as trip_distance,
-        passenger_count as passenger_count,
-        total_amount as total_amount
-    from 'data/raw/taxi_trips_2023-03.parquet'
-);
